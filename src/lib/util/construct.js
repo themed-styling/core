89178f21360f17bf1b870fc3772b71a6cd0dd117
aptt <a@ptt.li>
2020-05-08 11:06:05 +0200

    feat(modularity): 🎸 add modular functions
    add modular array construction with transformers and px transformer

---
+++ src/lib/util/construct.js
@@ -0,0 +1,39 @@
+import themeGet from '@styled-system/theme-get'
+import px from './transformers/px'
+
+/**
+ * Constructs an array for a given value and CSS property
+ *
+ * The array is made up of strings and {@link themeGet} calls.
+ *
+ * This function is an abstraction to be used for functions representing CSS properties.
+ *
+ * @param {(valueCollection|string|number|bigint)} value - The value to be used
+ * @param {theme} theme - A theme object passed by styled-components
+ * @param {string} cssProperty - The CSS property to be set
+ * @param {function((string|number|bigint))=} transformer - A function to transform the value depending on its type and the CSS property
+ * @default {@link px}
+ * @returns {Array.<(string|function(props))>} An array of strings and {@link themeGet} calls
+ */
+export default (value, theme, cssProperty, transformer = px) => {
+  switch (typeof value) {
+    case 'object':
+      return Object.entries(value).flatMap(([key, value]) => [
+        '@media screen and (min-width:',
+        theme.breakpoints[key],
+        '){',
+        cssProperty,
+        ':',
+        themeGet(value, transformer(value)),
+        ';}',
+      ])
+    case 'string':
+    case 'number':
+    case 'bigint':
+      return [cssProperty, ':', themeGet(value, transformer(value)), ';']
+    default:
+      throw new TypeError(
+        'Value has to be of type object, string, number or bigint'
+      )
+  }
+}
