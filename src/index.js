5fb3a5a2a1350d43e931e6048e0970478301aba0
aptt <a@ptt.li>
2020-05-08 12:29:03 +0200

    feat(package): 🎸 add separate margins
    add marginTop, marginLeft, marginBottom and marginRight to index

--- src/index.js
+++ src/index.js
@@ -1,2 +1,6 @@
 export { default as fontSize } from './lib/fontSize'
 export { default as margin } from './lib/margin/margin'
+export { default as marginTop } from './lib/margin/marginTop'
+export { default as marginLeft } from './lib/margin/marginLeft'
+export { default as marginBottom } from './lib/margin/marginBottom'
+export { default as marginRight } from './lib/margin/marginRight'
