0f252e95d885c5e0ec24de60d7810a61a900db6e
aptt <a@ptt.li>
2020-05-08 09:53:15 +0200

    docs(margin): ✏️ add documentation
    add jsdoc to margin

---
+++ src/typedef.js
@@ -0,0 +1,17 @@
+/**
+ * An object or array containing strings, numbers, bigints or any combination of them
+ * @typedef {(Object.<(string|number|bigint)>|Array.<(string|number|bigint)>)} valueCollection
+ */
+
+/**
+ * An object containing <code>breakpoints</code>
+ * @typedef {Object} theme
+ * @property {valueCollection} breakpoints - An object or array of CSS <code>min-width</code> values or numbers
+ */
+
+/**
+ * An object containing <code>margin</code> and <code>theme</code>
+ * @typedef {Object} props
+ * @property {(valueCollection|string|number|bigint)} margin - A CSS <code>margin</code> value, number or mixed collection of either
+ * @property {theme} theme - An object containing a <code>breakpoints</code> object or array
+ */
