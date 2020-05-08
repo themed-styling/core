c228e431bba2748d3812285f70772367287d0f2e
aptt <a@ptt.li>
2020-05-08 11:06:05 +0200

    feat(modularity): 🎸 add modular functions
    add modular array construction with transformers and px transformer

---
+++ src/lib/util/transformers/px.js
@@ -0,0 +1,26 @@
+/**
+ * Transforms numbers into px values and leaves strings as they are
+ *
+ * This is the default transformer for the <code>construct</code> function.
+ *
+ * @example
+ * const pxValue = px(100)
+ * const stringValue = px('1rem')
+ *
+ * pxValue === '100px'
+ * stringValue === '1rem'
+ *
+ * @param {(string|number|bigint)} value - The value to be transformed
+ * @returns {string} The transformed value as a string
+ */
+export default (value) => {
+  switch (typeof value) {
+    case 'number':
+    case 'bigint':
+      return `${value}px`
+    case 'string':
+      return value
+    default:
+      throw new TypeError('Value has to be of type string, number or bigint')
+  }
+}
