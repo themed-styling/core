bffad36e7da03c90cdd045268becaa6ccf8e010e
aptt <a@ptt.li>
2020-05-08 11:18:09 +0200

    feat(font-size): 🎸 add font size
    add font size function

--- src/index.js
+++ src/index.js
@@ -1,1 +1,2 @@
-export { default as margin } from './lib/margin/margin.js'
+export { default as fontSize } from './lib/fontSize'
+export { default as margin } from './lib/margin/margin'
---
+++ src/lib/fontSize.js
@@ -0,0 +1,21 @@
+import construct from './util/construct'
+
+/**
+ * Returns a function that takes an object containing fontSize and theme properties
+ *
+ * This function is meant to be used with styled-components. Call this function within your styled-component's template literal like so:
+ *
+ * @example
+ * const MyComponent = styled.div`
+ *   ${fontSize()}
+ * `
+ * @example
+ * const MyComponentWithFallback = styled.div`
+ *   ${margin({ breakpoint1: "1rem", breakpoint2: 18})}
+ * `
+ *
+ * @param {(Object<(string|number|bigint)>|string|number|bigint)=} fallback - A fallback value for when the object passed to the returned function does not contain a margin value
+ * @returns {function(props)} Component props passed by styled-components
+ */
+export default (fallback) => ({ fontSize, theme }) =>
+  construct(fontSize || fallback, theme, 'font-size')
