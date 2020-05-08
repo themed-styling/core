13c20adb08f8dd84c1158703895680dcb323a5d9
aptt <a@ptt.li>
2020-05-08 12:22:47 +0200

    feat(margin-left): 🎸 add margin left

---
+++ src/lib/margin/marginLeft.js
@@ -0,0 +1,21 @@
+import construct from '../util/construct'
+
+/**
+ * Returns a function that takes an object containing marginLeft and theme properties
+ *
+ * This function is meant to be used with styled-components. Call this function within your styled-component's template literal like so:
+ *
+ * @example
+ * const MyComponent = styled.div`
+ *   ${marginLeft()}
+ * `
+ * @example
+ * const MyComponentWithFallback = styled.div`
+ *   ${marginLeft({ breakpoint1: "1rem", breakpoint2: 10})}
+ * `
+ *
+ * @param {(valueCollection|string|number|bigint)=} fallback - A fallback value for when the object passed to the returned function does not contain a marginLeft value
+ * @returns {function(props)} Component props passed by styled-components
+ */
+export default (fallback) => ({ marginLeft, theme }) =>
+  construct(marginLeft || fallback, theme, 'margin-left')
