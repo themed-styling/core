c6913ad2024456c7ddc2eb7acd32a79e840f9f5c
aptt <a@ptt.li>
2020-05-08 09:53:15 +0200

    docs(margin): ✏️ add documentation
    add jsdoc to margin

--- src/margin/margin.js
+++ src/margin/margin.js
@@ -1,5 +1,22 @@
 import themeGet from '@styled-system/theme-get'
 
+/**
+ * Returns a function that takes an object containing margin and theme properties
+ *
+ * This function is meant to be used with styled-components. Call this function within your styled-component's template literal like so:
+ *
+ * @example
+ * const MyComponent = styled.div`
+ *   ${margin()}
+ * `
+ * @example
+ * const MyComponentWithFallback = styled.div`
+ *   ${margin({ breakpoint1: "1rem", breakpoint2: 10})}
+ * `
+ *
+ * @param {(Object<(string|number|bigint)>|string|number|bigint)=} fallback - A fallback value for when the object passed to the returned function does not contain a margin value
+ * @returns {function(props)} Component props passed by styled-components
+ */
 export default (fallback) => ({ margin, theme }) => {
   const value = margin || fallback
   switch (typeof value) {
