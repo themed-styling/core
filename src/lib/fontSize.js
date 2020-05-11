e7f673f45adaed6b59db3f26fa6e2d2459f829f2
aptt <a@ptt.li>
2020-05-11 13:18:56 +0200

    docs(font-size): ✏️ remove value collection and polish docs

--- src/lib/fontSize.js
+++ src/lib/fontSize.js
@@ -1,7 +1,7 @@
 import construct from './util/construct'
 
 /**
- * Returns a function that takes an object containing fontSize and theme properties
+ * Returns a function that takes an object containing fontSize and theme properties.
  *
  * This function is meant to be used with styled-components. Call this function within your styled-component's template literal like so:
  *
@@ -11,11 +11,13 @@ import construct from './util/construct'
  * `
  * @example
  * const MyComponentWithFallback = styled.div`
- *   ${fontSize({ breakpoint1: "1rem", breakpoint2: 18})}
+ *   ${fontSize({ breakpoint1: '1rem', breakpoint2: 18})}
  * `
  *
- * @param {(valueCollection|string|number|bigint)=} fallback - A fallback value for when the object passed to the returned function does not contain a margin value
+ * @param {(Object.<(string|number|bigint)>|Array.<(string|number|bigint)>|string|number|bigint)=} fallback - A fallback value for when the object passed to the returned function does not contain a margin value
  * @returns {function(props)} Component props passed by styled-components
+ * @name fontSize
+ * @memberOf core
  */
 export default (fallback) => ({ fontSize, theme }) =>
   construct(fontSize || fallback, theme, 'font-size')
