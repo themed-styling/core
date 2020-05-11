16731d4248c3011e77957f2044aed63a4ec4b3f8
aptt <a@ptt.li>
2020-05-11 20:42:16 +0200

    feat(core): 🎸 add justify-content

---
+++ src/lib/alignment/justifyContent.js
@@ -0,0 +1,23 @@
+import construct from '../util/construct'
+
+/**
+ * Returns a function that takes an object containing justifyContent and theme properties.
+ *
+ * This function is meant to be used with styled-components. Call this function within your styled-component's template literal like so:
+ *
+ * @example
+ * const MyComponent = styled.div`
+ *   ${justifyContent()}
+ * `
+ * @example
+ * const MyComponentWithFallback = styled.div`
+ *   ${justifyContent({ breakpoint1: '1rem', breakpoint2: 10})}
+ * `
+ *
+ * @param {(Object.<(string|number|bigint)>|Array.<(string|number|bigint)>|string|number|bigint)=} fallback - A fallback value for when the object passed to the returned function does not contain a justifyContent value
+ * @returns {function(props)} Component props passed by styled-components
+ * @name justifyContent
+ * @memberOf core
+ */
+export default (fallback) => ({ justifyContent, theme }) =>
+  construct(justifyContent || fallback, theme, 'justify-content')
