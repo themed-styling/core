0a7ccebfdd22a68eaff3bf0a746d0a111bace1d0
aptt <a@ptt.li>
2020-05-11 13:20:29 +0200

    docs(margin-top): ✏️ remove value collection and polish docs

--- src/lib/margin/marginTop.js
+++ src/lib/margin/marginTop.js
@@ -1,7 +1,7 @@
 import construct from '../util/construct'
 
 /**
- * Returns a function that takes an object containing marginTop and theme properties
+ * Returns a function that takes an object containing marginTop and theme properties.
  *
  * This function is meant to be used with styled-components. Call this function within your styled-component's template literal like so:
  *
@@ -11,11 +11,13 @@ import construct from '../util/construct'
  * `
  * @example
  * const MyComponentWithFallback = styled.div`
- *   ${marginTop({ breakpoint1: "1rem", breakpoint2: 10})}
+ *   ${marginTop({ breakpoint1: '1rem', breakpoint2: 10})}
  * `
  *
- * @param {(valueCollection|string|number|bigint)=} fallback - A fallback value for when the object passed to the returned function does not contain a marginTop value
+ * @param {(Object.<(string|number|bigint)>|Array.<(string|number|bigint)>|string|number|bigint)=} fallback - A fallback value for when the object passed to the returned function does not contain a marginTop value
  * @returns {function(props)} Component props passed by styled-components
+ * @name marginTop
+ * @memberOf core
  */
 export default (fallback) => ({ marginTop, theme }) =>
   construct(marginTop || fallback, theme, 'margin-top')
