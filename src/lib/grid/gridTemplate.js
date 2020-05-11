ee13f57370d2c4f1013a3f9c1d22b017163d9e80
aptt <a@ptt.li>
2020-05-11 23:09:41 +0200

    docs(gridTemplate): ✏️ fix examples

--- src/lib/grid/gridTemplate.js
+++ src/lib/grid/gridTemplate.js
@@ -10,8 +10,8 @@ import construct from '../util/construct'
  *   ${gridTemplate()}
  * `
  * @example
- * const MyComponentWithFallback = styled.div`
- *   ${gridTemplate({ breakpoint1: '1rem', breakpoint2: 18})}
+ * const MyComponent = styled.div`
+ *   ${gridTemplate({ breakpoint1: '1fr 2fr / auto', breakpoint2: 1fr / auto})}
  * `
  *
  * @param {(Object.<(string|number|bigint)>|Array.<(string|number|bigint)>|string|number|bigint)=} fallback - A fallback value for when the object passed to the returned function does not contain a margin value
