4e281f324ee280d88982bedf383b83077282c26b
aptt <a@ptt.li>
2020-05-11 13:26:20 +0200

    docs(margin-left): ✏️ remove value collection and polish docs

--- src/lib/margin/marginLeft.js
+++ src/lib/margin/marginLeft.js
@@ -1,7 +1,7 @@
 import construct from '../util/construct'
 
 /**
- * Returns a function that takes an object containing marginLeft and theme properties
+ * Returns a function that takes an object containing marginLeft and theme properties.
  *
  * This function is meant to be used with styled-components. Call this function within your styled-component's template literal like so:
  *
@@ -11,11 +11,13 @@ import construct from '../util/construct'
  * `
  * @example
  * const MyComponentWithFallback = styled.div`
- *   ${marginLeft({ breakpoint1: "1rem", breakpoint2: 10})}
+ *   ${marginLeft({ breakpoint1: '1rem', breakpoint2: 10})}
  * `
  *
- * @param {(valueCollection|string|number|bigint)=} fallback - A fallback value for when the object passed to the returned function does not contain a marginLeft value
+ * @param {(Object.<(string|number|bigint)>|Array.<(string|number|bigint)>|string|number|bigint)=} fallback - A fallback value for when the object passed to the returned function does not contain a marginLeft value
  * @returns {function(props)} Component props passed by styled-components
+ * @name marginLeft
+ * @memberOf core
  */
 export default (fallback) => ({ marginLeft, theme }) =>
   construct(marginLeft || fallback, theme, 'margin-left')
