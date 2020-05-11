65813962b089e6c51f7586467592420ef44d66f3
aptt <a@ptt.li>
2020-05-11 13:30:13 +0200

    docs(index): ✏️ move docs to index
    move docs from typedef.js to index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,25 @@
+/**
+ * An object containing <code>breakpoints</code>
+ * @typedef {Object} theme
+ * @property {(Object.<(string|number|bigint)>|Array.<(string|number|bigint)>)} breakpoints - An object or array of CSS <code>min-width</code> values or numbers
+ */
+
+/**
+ * An object containing {@link theme}
+ * @typedef {Object} props
+ * @property {theme} theme - An object containing a <code>breakpoints</code> object or array
+ */
+
+/**
+ * Core functions
+ * @namespace core
+ */
+
+/**
+ * Utility Functions
+ * @namespace util
+ */
+
 export { default as fontSize } from './lib/fontSize'
 export { default as margin } from './lib/margin/margin'
 export { default as marginTop } from './lib/margin/marginTop'
