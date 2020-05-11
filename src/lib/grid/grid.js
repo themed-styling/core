2ec8fc062d1eb9cb7e83505682ef8e6e58389015
aptt <a@ptt.li>
2020-05-11 22:48:17 +0200

    fix(grid): 🐛 use plain transformer

--- src/lib/grid/grid.js
+++ src/lib/grid/grid.js
@@ -1,4 +1,5 @@
 import construct from '../util/construct'
+import plain from '../util/transformers/plain'
 
 /**
  * Returns a function that takes an object containing grid and theme properties.
@@ -20,4 +21,4 @@ import construct from '../util/construct'
  * @memberOf core
  */
 export default (fallback) => ({ grid, theme }) =>
-  construct(grid || fallback, theme, 'grid')
+  construct(grid || fallback, theme, 'grid', plain)
