ba05bba4296bca8d273b997a672c8790e6972761
aptt <a@ptt.li>
2020-05-11 20:33:02 +0200

    feat(core): 🎸 add grid-column-start

---
+++ src/lib/grid/gridColumnStart.js
@@ -0,0 +1,23 @@
+import construct from '../util/construct'
+
+/**
+ * Returns a function that takes an object containing gridColumnStart and theme properties.
+ *
+ * This function is meant to be used with styled-components. Call this function within your styled-component's template literal like so:
+ *
+ * @example
+ * const MyComponent = styled.div`
+ *   ${gridColumnStart()}
+ * `
+ * @example
+ * const MyComponentWithFallback = styled.div`
+ *   ${gridColumnStart({ breakpoint1: '1rem', breakpoint2: 18})}
+ * `
+ *
+ * @param {(Object.<(string|number|bigint)>|Array.<(string|number|bigint)>|string|number|bigint)=} fallback - A fallback value for when the object passed to the returned function does not contain a margin value
+ * @returns {function(props)} Component props passed by styled-components
+ * @name gridColumnStart
+ * @memberOf core
+ */
+export default (fallback) => ({ gridColumnStart, theme }) =>
+  construct(gridColumnStart || fallback, theme, 'grid-column-start')
