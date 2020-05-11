b52547ee706d121b824638fdf90ad8efc157c672
aptt <a@ptt.li>
2020-05-11 22:50:43 +0200

    fix(gridRowEnd): 🐛 use plain transformer

--- src/lib/grid/gridRowEnd.js
+++ src/lib/grid/gridRowEnd.js
@@ -1,4 +1,5 @@
 import construct from '../util/construct'
+import plain from '../util/transformers/plain'
 
 /**
  * Returns a function that takes an object containing gridRowEnd and theme properties.
@@ -20,4 +21,4 @@ import construct from '../util/construct'
  * @memberOf core
  */
 export default (fallback) => ({ gridRowEnd, theme }) =>
-  construct(gridRowEnd || fallback, theme, 'grid-row-end')
+  construct(gridRowEnd || fallback, theme, 'grid-row-end', plain)
