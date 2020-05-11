366c56727078cbc1c75d84fe5e0b5fdc3d5f932f
aptt <a@ptt.li>
2020-05-11 21:45:25 +0200

    fix(color): 🐛 add color transformer

--- src/lib/color.js
+++ src/lib/color.js
@@ -1,4 +1,5 @@
 import construct from './util/construct'
+import _color from './util/transformers/color'
 
 /**
  * Returns a function that takes an object containing color and theme properties.
@@ -20,4 +21,4 @@ import construct from './util/construct'
  * @memberOf core
  */
 export default (fallback) => ({ color, theme }) =>
-  construct(color || fallback, theme, 'color')
+  construct(color || fallback, theme, 'color', _color)
