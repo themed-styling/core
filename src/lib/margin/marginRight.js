60c81b41924d592f40ca5481c020a4971a785bd8
aptt <a@ptt.li>
2020-05-12 13:00:28 +0200

    docs(marginRight): ✏️ fix examples

--- src/lib/margin/marginRight.js
+++ src/lib/margin/marginRight.js
@@ -10,8 +10,16 @@ import construct from '../util/construct'
  *   ${marginRight()}
  * `
  * @example
- * const MyComponentWithFallback = styled.div`
- *   ${marginRight({ breakpoint1: '1rem', breakpoint2: 10})}
+ * const MyComponent = styled.div`
+ *   ${marginRight(20)}
+ * `
+ * @example
+ * const MyComponent = styled.div`
+ *   ${marginRight('1.5rem')}
+ * `
+ * @example
+ * const MyComponent = styled.div`
+ *   ${marginRight({ breakpoint1: 20, breakpoint2: '1.5rem'})}
  * `
  *
  * @param {(Object.<(string|number|bigint)>|Array.<(string|number|bigint)>|string|number|bigint)=} fallback - A fallback value for when the object passed to the returned function does not contain a marginRight value
