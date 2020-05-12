1f9b2f92affc68c223c93b7690e1c1c423694143
aptt <a@ptt.li>
2020-05-12 19:02:11 +0200

    feat(package): 🎸 add css function
    add css function which allows to set raw css

---
+++ src/lib/css.js
@@ -0,0 +1,54 @@
+import construct from './util/construct'
+import themeGet from '@styled-system/theme-get'
+
+/**
+ * Returns a function that takes an object containing css and theme properties.
+ *
+ * This function is meant to be used with styled-components. Call this function within your styled-component's template literal like so:
+ *
+ * @example
+ * const MyComponent = styled.div`
+ *   ${css()}
+ * `
+ * @example
+ * const MyComponent = styled.div`
+ *   ${css('width: 300px;')}
+ * `
+ * @example
+ * const MyComponent = styled.div`
+ *   ${css({
+ *     breakpoint1: `width: 300px;
+ *     height: 500px;`,
+ *     breakpoint2: `width: 500px;
+ *     height: 300px;`
+ *   })}
+ * `
+ *
+ * @param {(Object.<(string|number|bigint)>|Array.<(string|number|bigint)>|string|number|bigint)=} fallback - A fallback value for when the object passed to the returned function does not contain a margin value
+ * @returns {function(props)} Function to take component props passed by styled-components
+ * @name css
+ * @memberOf core
+ */
+export default (fallback) => ({ css, theme }) => {
+  const value = css || fallback
+  switch (typeof value) {
+    case 'object':
+      const { standard, ...breakpoints } = value
+      return [
+        standard && [1, 2, 3],
+        ...Object.entries(breakpoints).flatMap(([key, value]) => [
+          '@media screen and (min-width:',
+          theme.breakpoints[key],
+          '){',
+          value,
+          ';}',
+        ]),
+      ].flat()
+    case 'string':
+      return value
+    case 'undefined':
+      return null
+    default:
+      throw new TypeError('Value has to be of type object or string')
+  }
+}
