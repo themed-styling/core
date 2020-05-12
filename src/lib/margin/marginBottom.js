43a0c38ce59c82a3b4e04e5b32d9aab2fa50d637
aptt <a@ptt.li>
2020-05-12 12:59:35 +0200

    docs(marginBottom): ✏️ fix examples

--- src/lib/margin/marginBottom.js
+++ src/lib/margin/marginBottom.js
@@ -10,8 +10,16 @@ import construct from '../util/construct'
  *   ${marginBottom()}
  * `
  * @example
- * const MyComponentWithFallback = styled.div`
- *   ${marginBottom({ breakpoint1: '1rem', breakpoint2: 10})}
+ * const MyComponent = styled.div`
+ *   ${marginBottom(20)}
+ * `
+ * @example
+ * const MyComponent = styled.div`
+ *   ${marginBottom('1.5rem')}
+ * `
+ * @example
+ * const MyComponent = styled.div`
+ *   ${marginBottom({ breakpoint1: 20, breakpoint2: '1.5rem'})}
  * `
  *
  * @param {(Object.<(string|number|bigint)>|Array.<(string|number|bigint)>|string|number|bigint)=} fallback - A fallback value for when the object passed to the returned function does not contain a marginBottom value
