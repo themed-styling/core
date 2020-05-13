9f40c06716866ed7f0c9b752a8062a4fff2f0aac
aptt <a@ptt.li>
2020-05-13 10:48:28 +0200

    feat(core): 🎸 add minHeight

---
+++ src/lib/dimensions/minHeight.js
@@ -0,0 +1,31 @@
+import construct from '../util/construct'
+
+/**
+ * Returns a function that takes an object containing minHeight and theme properties.
+ *
+ * This function is meant to be used with styled-components. Call this function within your styled-component's template literal like so:
+ *
+ * @example
+ * const MyComponent = styled.div`
+ *   ${minHeight()}
+ * `
+ * @example
+ * const MyComponent = styled.div`
+ *   ${minHeight(300)}
+ * `
+ * @example
+ * const MyComponent = styled.div`
+ *   ${minHeight('50vh')}
+ * `
+ * @example
+ * const MyComponent = styled.div`
+ *   ${minHeight({ breakpoint1: 300, breakpoint2: '50vh'})}
+ * `
+ *
+ * @param {(Object.<(string|number|bigint)>|Array.<(string|number|bigint)>|string|number|bigint)=} fallback - A fallback value for when the object passed to the returned function does not contain a margin value
+ * @returns {function(props)} Function to take component props passed by styled-components
+ * @name minHeight
+ * @memberOf core
+ */
+export default (fallback) => ({ minHeight, theme }) =>
+  construct(minHeight || fallback, theme, 'min-height')
