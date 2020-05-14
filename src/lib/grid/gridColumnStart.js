b3500ef851a80578a4bb2ec221fc26be45143f44
aptt <a@ptt.li>
2020-05-15 01:01:18 +0200

    docs(core): ✏️ fix returns type definition
    ✅ Closes: #49

--- src/lib/grid/gridColumnStart.js
+++ src/lib/grid/gridColumnStart.js
@@ -21,7 +21,7 @@ import plain from '../util/transformers/plain'
  *
  * @param {(Object.<(string|number|bigint)>|Array.<(string|number|bigint)>|string|number|bigint)=} fallback - A fallback value for when the object passed to the returned function does not contain a margin value
  * @param {boolean=} [propless=false] - Whether the component should be without prop
- * @returns {function(props)} Function to take component props passed by styled-components
+ * @returns {function({props})} Function to take component props passed by styled-components
  * @name gridColumnStart
  * @memberOf core
  */
