07ac33676d1b0350ef8f79c7142f1cfd7d697134
aptt <a@ptt.li>
2020-05-14 19:02:02 +0200

    feat(minHeight): 🎸 add propless parameter
    add the ability to specify whether or not the component should have a
    prop

--- src/lib/dimensions/minHeight.js
+++ src/lib/dimensions/minHeight.js
@@ -23,9 +23,13 @@ import construct from '../util/construct'
  * `
  *
  * @param {(Object.<(string|number|bigint)>|Array.<(string|number|bigint)>|string|number|bigint)=} fallback - A fallback value for when the object passed to the returned function does not contain a margin value
+ * @param {boolean=} [propless=false] - Whether the component should be without prop
  * @returns {function(props)} Function to take component props passed by styled-components
  * @name minHeight
  * @memberOf core
  */
-export default (fallback) => ({ minHeight, theme }) =>
-  construct(minHeight || fallback, theme, 'min-height')
+export default (fallback, propless = false) =>
+  propless
+    ? ({ theme }) => construct(fallback, theme, 'min-height')
+    : ({ minHeight, theme }) =>
+        construct(minHeight || fallback, theme, 'min-height')
