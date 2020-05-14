49f00f7b69a47036223de8d9150fcfe18b2e15c0
aptt <a@ptt.li>
2020-05-14 20:04:11 +0200

    feat(marginLeft): 🎸 add propless parameter
    add the ability to specify whether or not the component should have a
    prop

--- src/lib/margin/marginLeft.js
+++ src/lib/margin/marginLeft.js
@@ -23,9 +23,13 @@ import construct from '../util/construct'
  * `
  *
  * @param {(Object.<(string|number|bigint)>|Array.<(string|number|bigint)>|string|number|bigint)=} fallback - A fallback value for when the object passed to the returned function does not contain a marginLeft value
+ * @param {boolean=} [propless=false] - Whether the component should be without prop
  * @returns {function(props)} Function to take component props passed by styled-components
  * @name marginLeft
  * @memberOf core
  */
-export default (fallback) => ({ marginLeft, theme }) =>
-  construct(marginLeft || fallback, theme, 'margin-left')
+export default (fallback, propless = false) =>
+  propless
+    ? ({ theme }) => construct(fallback, theme, 'margin-left')
+    : ({ marginLeft, theme }) =>
+        construct(marginLeft || fallback, theme, 'margin-left')
