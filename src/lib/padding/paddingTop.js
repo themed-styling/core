43496f58eba99f7b9885faa215f4d573191c558c
aptt <a@ptt.li>
2020-05-15 00:00:35 +0200

    feat(paddingTop): 🎸 add propless parameter
    add the ability to specify whether or not the component should have a
    prop

--- src/lib/padding/paddingTop.js
+++ src/lib/padding/paddingTop.js
@@ -23,9 +23,13 @@ import construct from '../util/construct'
  * `
  *
  * @param {(Object.<(string|number|bigint)>|Array.<(string|number|bigint)>|string|number|bigint)=} fallback - A fallback value for when the object passed to the returned function does not contain a paddingTop value
+ * @param {boolean=} [propless=false] - Whether the component should be without prop
  * @returns {function(props)} Function to take component props passed by styled-components
  * @name paddingTop
  * @memberOf core
  */
-export default (fallback) => ({ paddingTop, theme }) =>
-  construct(paddingTop || fallback, theme, 'padding-top')
+export default (fallback, propless = false) =>
+  propless
+    ? ({ theme }) => construct(fallback, theme, 'padding-top')
+    : ({ paddingTop, theme }) =>
+        construct(paddingTop || fallback, theme, 'padding-top')
