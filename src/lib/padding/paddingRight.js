04c48ccfaabdde2a01289ff28eb176eaf6facf03
aptt <a@ptt.li>
2020-05-15 00:00:21 +0200

    feat(paddingRight): 🎸 add propless parameter
    add the ability to specify whether or not the component should have a
    prop

--- src/lib/padding/paddingRight.js
+++ src/lib/padding/paddingRight.js
@@ -23,9 +23,13 @@ import construct from '../util/construct'
  * `
  *
  * @param {(Object.<(string|number|bigint)>|Array.<(string|number|bigint)>|string|number|bigint)=} fallback - A fallback value for when the object passed to the returned function does not contain a paddingRight value
+ * @param {boolean=} [propless=false] - Whether the component should be without prop
  * @returns {function(props)} Function to take component props passed by styled-components
  * @name paddingRight
  * @memberOf core
  */
-export default (fallback) => ({ paddingRight, theme }) =>
-  construct(paddingRight || fallback, theme, 'padding-right')
+export default (fallback, propless = false) =>
+  propless
+    ? ({ theme }) => construct(fallback, theme, 'padding-right')
+    : ({ paddingRight, theme }) =>
+        construct(paddingRight || fallback, theme, 'padding-right')
