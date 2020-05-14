49a4e2be6a3568f15f5b85218e6270428d94f8c4
aptt <a@ptt.li>
2020-05-14 19:01:06 +0200

    feat(height): 🎸 add propless parameter
    add the ability to specify whether or not the component should have a
    prop

--- src/lib/dimensions/height.js
+++ src/lib/dimensions/height.js
@@ -23,9 +23,12 @@ import construct from '../util/construct'
  * `
  *
  * @param {(Object.<(string|number|bigint)>|Array.<(string|number|bigint)>|string|number|bigint)=} fallback - A fallback value for when the object passed to the returned function does not contain a margin value
+ * @param {boolean=} [propless=false] - Whether the component should be without prop
  * @returns {function(props)} Function to take component props passed by styled-components
  * @name height
  * @memberOf core
  */
-export default (fallback) => ({ height, theme }) =>
-  construct(height || fallback, theme, 'height')
+export default (fallback, propless = false) =>
+  propless
+    ? ({ theme }) => construct(fallback, theme, 'height')
+    : ({ height, theme }) => construct(height || fallback, theme, 'height')
