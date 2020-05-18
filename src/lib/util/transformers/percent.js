50b8c23efd31d2612ba3f87c20cc2d17fe3ff7b4
aptt <a@ptt.li>
2020-05-18 22:13:55 +0200

    feat(util): 🎸 add percent transformer

---
+++ src/lib/util/transformers/percent.js
@@ -0,0 +1,29 @@
+/**
+ * Transforms numbers into percentages and leaves strings as they are.
+ *
+ * Numbers below 1 are multiplied by 100.
+ *
+ * @example
+ * const percentage = percent(0.5)    // 50%
+ * @example
+ * const percentage = percent(50)     // 50%
+ *
+ * @param {(string|number|bigint)} value - The value to be transformed
+ * @returns {(string|Array.<string>)} The transformed value as a string or an array
+ * @name percent
+ * @memberOf util
+ */
+export default (value) => {
+  switch (typeof value) {
+    case 'number':
+    case 'bigint':
+      if (value <= 1) {
+        return [value * 100, '%']
+      }
+      return [value, '%']
+    case 'string':
+      return value
+    default:
+      throw new TypeError('Value has to be of type string, number or bigint')
+  }
+}
