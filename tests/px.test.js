b0b19b59d4f664562817d59d5c84a4860a0db5de
aptt <a@ptt.li>
2020-05-18 22:50:53 +0200

    test(px): 💍 add px test

---
+++ tests/px.test.js
@@ -0,0 +1,9 @@
+import px from '../src/lib/util/transformers/px'
+
+test('transforms number into px value', () => {
+  expect(px(10)).toBe('10px')
+})
+
+test('leaves string values be', () => {
+  expect('asdasd').toBe('asdasd')
+})
