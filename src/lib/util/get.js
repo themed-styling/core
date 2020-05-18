4aaa5271ea3ea6e23ecc8de017e9ac3389deca57
aptt <a@ptt.li>
2020-05-18 15:49:23 +0200

    feat(get): 🎸 add error handling
    get now returns null when it encounters an error

--- src/lib/util/get.js
+++ src/lib/util/get.js
@@ -1,12 +1,3 @@
-/**
- * Gets a value from an object based on a string path.
- *
- * @param {string} path - The value's path
- * @param {object} object - The theme
- * @returns A value, undefined or null
- * @name get
- * @memberOf util
- */
 const get = (path, object) => {
   const split = path.split(/\.(.+)/)
   if (split.length > 1) {
@@ -16,4 +7,19 @@ const get = (path, object) => {
   }
 }
 
-export default get
+/**
+ * Gets a value from an object based on a string path.
+ *
+ * @param {string} path - The value's path
+ * @param {object} object - The theme
+ * @returns A value, undefined or null
+ * @name get
+ * @memberOf util
+ */
+export default (path, object) => {
+  try {
+    return get(path, object)
+  } catch (e) {
+    return null
+  }
+}
