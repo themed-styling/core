a9c77fb9c94dbfff8a3f4387901b74e0c0b194cb
aptt <a@ptt.li>
2020-07-23 16:11:47 +0200

    refactor(propless): 💡 to create core functions
    propless now creates core functions that don't take a prop
    
    BREAKING CHANGE: 🧨 propless' purpose was changed

--- src/lib/util/propless.js
+++ src/lib/util/propless.js
@@ -1,15 +1,59 @@
+import px from './transformers/px'
+import get from './get'
+
 /**
- * Adds a parameter to specify whether or not a component should have a prop.
+ * Universal function to create propless functions.
  *
- * Takes a callback to construct the array and the name of the prop the component will have, should it have a prop.
+ * Returns a function that takes a value and returns a function which
+ * takes an object containing a theme property. This function returns an array
+ * based on the value.
  *
- * @param {function((Object.<(string|number|bigint)>|Array.<(string|number|bigint)>|string|number|bigint)=, theme)} callback - A callback to pass a value and {@link theme} to
- * @param {string} propName - The name of the prop the function that is returned by the returned function should have
- * @returns {function((Object.<(string|number|bigint)>|Array.<(string|number|bigint)>|string|number|bigint)=, boolean=)} A function that takes a fallback value and flag whether the returned function should return a function that takes a prop
+ * @param {string|Array.<string>} cssLineStart - The CSS code before the value
+ * @param {string|Array.<string>} cssLineTerminate - The CSS code after the value
+ * @param {function((string|number|bigint))=} transformer - A function to transform the value depending on its type and the CSS property
+ * @default {@link px}
+ * @returns {function((Object.<(string|number|bigint)>|Array.<(string|number|bigint)>|string|number|bigint))} A function that takes a props object and returns an array
  * @name propless
  * @memberOf util
  */
-export default (callback, propName) => (fallback, propless = false) =>
-  propless
-    ? ({ theme }) => callback(fallback, theme)
-    : ({ theme, ...props }) => callback(props[propName] || fallback, theme)
+export default (cssLineStart, cssLineTerminate, transformer = px) => value => ({
+  theme,
+}) => {
+  const _value = get(value, theme) || value
+  switch (typeof _value) {
+    case 'object':
+      const { standard, ...breakpoints } = _value
+      return [
+        standard !== undefined && [
+          cssLineStart,
+          get(standard, theme) || transformer(standard),
+          cssLineTerminate,
+        ],
+        ...Object.entries(breakpoints).map(([key, value]) => [
+          (theme.mediaQueries && theme.mediaQueries[key]) || [
+            '@media screen and (min-width:',
+            theme.breakpoints[key],
+            ')',
+          ],
+          '{',
+          cssLineStart,
+          get(value, theme) || transformer(value),
+          cssLineTerminate,
+          '}',
+        ]),
+      ]
+    case 'string':
+    case 'number':
+    case 'bigint':
+      return [
+        cssLineStart,
+        get(_value, theme) || transformer(_value),
+        cssLineTerminate,
+      ]
+    case 'undefined':
+    default:
+      throw new TypeError(
+        `Value has to be of type object, string, number or bigint but got ${_value}`
+      )
+  }
+}
