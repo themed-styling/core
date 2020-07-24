856f77a5c2281b909394c3ccbc2d155c3f85397e
aptt <a@ptt.li>
2020-07-24 15:26:05 +0200

    fix(propless): 🐛 doc comment

--- src/lib/util/propless.js
+++ src/lib/util/propless.js
@@ -7,11 +7,8 @@ import proplessConstructor from './constructors/proplessConstructor'
  * takes an object containing a theme property. This function returns an array
  * based on the value.
  *
- * @param {string|Array.<string>} cssLineStart - The CSS code before the value
- * @param {string|Array.<string>} cssLineTerminate - The CSS code after the value
- * @param {function((string|number|bigint))=} transformer - A function to transform the value depending on its type and the CSS property
- * @default {@link px}
- * @returns {function((Object.<(string|number|bigint)>|Array.<(string|number|bigint)>|string|number|bigint))} A function that takes a props object and returns an array
+ * @param {cssMaker} cssMaker - A function that takes a CSS value and returns an array
+ * @returns {proplessFunction} A function that takes a value
  * @name propless
  * @memberOf util
  */
