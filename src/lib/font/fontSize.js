d9b93ce233a987635916ba2d7a126e3305375213
aptt <a@ptt.li>
2020-07-24 14:52:05 +0200

    refactor(fontSize): 💡 add properties

--- src/lib/font/fontSize.js
+++ src/lib/font/fontSize.js
@@ -1,5 +1,6 @@
+import core from './util/core'
 import propless from './util/propless'
-import construct from './util/construct'
+import defaultMaker from './util/makers/defaultMaker'
 
 /**
  * Returns a function that takes an object containing fontSize and theme properties.
@@ -23,13 +24,25 @@ import construct from './util/construct'
  *   ${fontSize({ breakpoint1: 16, breakpoint2: '1.5rem'})}
  * `
  *
- * @param {(Object.<(string|number|bigint)>|Array.<(string|number|bigint)>|string|number|bigint)=} fallback - A fallback value for when the object passed to the returned function does not contain a margin value
- * @param {boolean=} [propless=false] - Whether the component should be without prop
- * @returns {function({props})} Function to take component props passed by styled-components
+ * @type {coreFunction}
+ * @property {coreFunction} important - Function to add styling and prop, and mark the value as !important
+ * @property {coreFunction} i - Shorthand for .important
+ * @property {proplessFunction} propless - Function to add styling without adding a prop
+ * @property {proplessFunction} l - Shorthand for .propless
+ * @property {proplessFunction} propless.important - Function to add styling without adding a prop and mark the value as !important
+ * @property {proplessFunction} l.i - Shorthand for .propless.important
  * @name fontSize
- * @memberOf core
+ * @memberOf core.font
  */
-export default propless(
-  (value, theme) => construct(value, theme, 'font-size'),
-  'fontSize'
+const fontSize = core('fontSize', defaultMaker('font-size:')()())
+fontSize.important = fontSize.i = core(
+  'fontSize',
+  defaultMaker('font-size:')()('!important;')
 )
+
+fontSize.propless = fontSize.l = propless(defaultMaker('font-size:')()())
+fontSize.propless.important = fontSize.l.i = propless(
+  defaultMaker('font-size:')()('!important;')
+)
+
+export default fontSize
