3c9e1dedafad6f38a5b227c28775cd94a5542e27
aptt <a@ptt.li>
2020-07-27 19:47:00 +0200

    test(color): 💍 refactor testing, using new functions

--- tests/color.test.js
+++ tests/color.test.js
@@ -2,10 +2,12 @@ import { color } from '../src/'
 import {
   testStringValuesOn,
   testNumberValuesOn,
+  testObjectValuesOn,
   testIllegalValuesOn,
 } from './utilities'
 
 color.name_ = 'color'
 testStringValuesOn(color, 'color:', ';', '!important;')
 testNumberValuesOn(color, 'color:', ';', '!important;', value => `#${value}`)
+testObjectValuesOn(color, 'color:', ';', '!important;', value => `#${value}`)
 testIllegalValuesOn(color)
