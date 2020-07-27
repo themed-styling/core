e9fc436f529f9c77ec06858c9dc4595d6606d1cb
aptt <a@ptt.li>
2020-07-27 17:45:47 +0200

    refactor(index-template): 💡 sort functions before mapping

--- scripts/util/templates/indexTemplate.js
+++ scripts/util/templates/indexTemplate.js
@@ -1,5 +1,10 @@
 export default functions =>
   functions
+    .sort(({ name: nameA }, { name: nameB }) => {
+      if (nameA < nameB) return -1
+      if (nameA > nameB) return 1
+      return 0
+    })
     .map(({ name, namespace }) =>
       namespace
         ? `export { default as ${name} } from './${namespace}/${name}'`
