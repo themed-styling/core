f1ab7d9f2a7cb4def9cf8e9e9cdd6547c677f0be
aptt <a@ptt.li>
2020-11-02 17:23:48 +0100

    test(component-test): 💍 fix component test using 2+gb of memory
    and use random word strings without dashes

--- tests/component.test.js
+++ tests/component.test.js
@@ -5,6 +5,7 @@ import faker from 'faker'
 import 'jest-styled-components'
 
 import * as themedStyling from '../src'
+import { testElement } from 'domutils'
 
 const { propType, css, ...functions } = themedStyling
 
@@ -39,136 +40,175 @@ console.info(`🌱 ${seed}`)
 faker.seed(seed)
 
 const breakpoints = {
-  smallCustomBreakpoint: faker.random.word(),
-  mobile: faker.random.word(),
-  mediumCustomBreakpoint: faker.random.word(),
-  tablet: faker.random.word(),
-  desktop: faker.random.word(),
-  wide: faker.random.word(),
-  largeCustomBreakpoint: faker.random.word(),
+  smallCustomBreakpoint: faker.date.month(),
+  mobile: faker.date.month(),
+  mediumCustomBreakpoint: faker.date.month(),
+  tablet: faker.date.month(),
+  desktop: faker.date.month(),
+  wide: faker.date.month(),
+  largeCustomBreakpoint: faker.date.month(),
 }
 
 const mediaQueries = {
-  smallCustomBreakpoint: `@media ${faker.random.word()}`,
-  mobile: `@media ${faker.random.word()}`,
-  mediumCustomBreakpoint: `@media ${faker.random.word()}`,
-  tablet: `@media ${faker.random.word()}`,
-  desktop: `@media ${faker.random.word()}`,
-  wide: `@media ${faker.random.word()}`,
-  largeCustomBreakpoint: `@media ${faker.random.word()}`,
+  smallCustomBreakpoint: `@media ${faker.date.month()}`,
+  mobile: `@media ${faker.date.month()}`,
+  mediumCustomBreakpoint: `@media ${faker.date.month()}`,
+  tablet: `@media ${faker.date.month()}`,
+  desktop: `@media ${faker.date.month()}`,
+  wide: `@media ${faker.date.month()}`,
+  largeCustomBreakpoint: `@media ${faker.date.month()}`,
 }
 
-const content = faker.random.word()
+for (const [name, fn] of Object.entries(functions)) {
+  test(`${name} renders number values`, () => {
+    const Div = styled.div`
+      ${fn(faker.random.number())}
+    `
 
-const number = faker.random.number()
-const NumberDiv = styled.div`
-  ${functionsArray.map(fn => fn(number))}
-`
+    const render = renderer.create(<Div>{faker.random.words()}</Div>).toJSON()
 
-const string = faker.random.word()
-const StringDiv = styled.div`
-  ${functionsArray.map(fn => fn(string))}
-`
+    expect(render).toMatchSnapshot()
+  })
 
-const numberObject = JSON.parse(
-  `{${faker.fake(
-    '"smallCustomBreakpoint": {{random.number}}, "mobile": {{random.number}}, "mediumCustomBreakpoint": {{random.number}}, "tablet": {{random.number}}, "desktop": {{random.number}}, "wide": {{random.number}}, "largeCustomBreakpoint": {{random.number}}, "standard": {{random.number}}'
-  )}}`
-)
-const NumberObjectDiv = styled.div`
-  ${functionsArray.map(fn => fn(numberObject))}
-`
+  test(`${name} renders string values`, () => {
+    const Div = styled.div`
+      ${fn(faker.date.month())}
+    `
 
-const stringObject = JSON.parse(
-  `{${faker.fake(
-    '"smallCustomBreakpoint": "{{random.word}}", "mobile": "{{random.word}}", "mediumCustomBreakpoint": "{{random.word}}", "tablet": "{{random.word}}", "desktop": "{{random.word}}", "wide": "{{random.word}}", "largeCustomBreakpoint": "{{random.word}}", "standard": "{{random.word}}"'
-  )}}`
-)
-const StringObjectDiv = styled.div`
-  ${functionsArray.map(fn => fn(stringObject))}
-`
+    const render = renderer.create(<Div>{faker.random.words()}</Div>).toJSON()
 
-const mixedObject = JSON.parse(
-  `{${faker.fake(
-    '"smallCustomBreakpoint": "{{random.number}}", "mobile": "{{random.word}}", "mediumCustomBreakpoint": "{{random.number}}", "tablet": "{{random.word}}", "desktop": "{{random.number}}", "wide": "{{random.word}}", "largeCustomBreakpoint": "{{random.number}}", "standard": "{{random.word}}"'
-  )}}`
-)
-const MixedObjectDiv = styled.div`
-  ${functionsArray.map(fn => fn(mixedObject))}
-`
+    expect(render).toMatchSnapshot()
+  })
 
-it('renders number values', () => {
-  const render = renderer.create(<NumberDiv>{content}</NumberDiv>).toJSON()
-  expect(render).toMatchSnapshot()
-})
+  test(`${name} renders number object values using breakpoints`, () => {
+    const Div = styled.div`
+      ${fn(
+        JSON.parse(
+          `{${faker.fake(
+            '"smallCustomBreakpoint": {{random.number}}, "mobile": {{random.number}}, "mediumCustomBreakpoint": {{random.number}}, "tablet": {{random.number}}, "desktop": {{random.number}}, "wide": {{random.number}}, "largeCustomBreakpoint": {{random.number}}, "standard": {{random.number}}'
+          )}}`
+        )
+      )}
+    `
 
-it('renders string values', () => {
-  const render = renderer.create(<StringDiv>{content}</StringDiv>).toJSON()
-  expect(render).toMatchSnapshot()
-})
+    const render = renderer
+      .create(
+        <ThemeProvider theme={{ ...theme, breakpoints }}>
+          <Div>{faker.random.words()}</Div>
+        </ThemeProvider>
+      )
+      .toJSON()
 
-it('renders number object with breakpoints', () => {
-  const render = renderer
-    .create(
-      <ThemeProvider theme={{ ...theme, breakpoints }}>
-        <NumberObjectDiv>{content}</NumberObjectDiv>
-      </ThemeProvider>
-    )
-    .toJSON()
-  expect(render).toMatchSnapshot()
-})
+    expect(render).toMatchSnapshot()
+  })
 
-it('renders string object with breakpoints', () => {
-  const render = renderer
-    .create(
-      <ThemeProvider theme={{ ...theme, breakpoints }}>
-        <StringObjectDiv>{content}</StringObjectDiv>
-      </ThemeProvider>
-    )
-    .toJSON()
-  expect(render).toMatchSnapshot()
-})
+  test(`${name} renders string object values using breakpoints`, () => {
+    const Div = styled.div`
+      ${fn(
+        JSON.parse(
+          `{${faker.fake(
+            '"smallCustomBreakpoint": "{{date.month}}", "mobile": "{{date.month}}", "mediumCustomBreakpoint": "{{date.month}}", "tablet": "{{date.month}}", "desktop": "{{date.month}}", "wide": "{{date.month}}", "largeCustomBreakpoint": "{{date.month}}", "standard": "{{date.month}}"'
+          )}}`
+        )
+      )}
+    `
 
-it('renders mixed object with breakpoints', () => {
-  const render = renderer
-    .create(
-      <ThemeProvider theme={{ ...theme, breakpoints }}>
-        <MixedObjectDiv>{content}</MixedObjectDiv>
-      </ThemeProvider>
-    )
-    .toJSON()
-  expect(render).toMatchSnapshot()
-})
+    const render = renderer
+      .create(
+        <ThemeProvider theme={{ ...theme, breakpoints }}>
+          <Div>{faker.random.words()}</Div>
+        </ThemeProvider>
+      )
+      .toJSON()
 
-it('renders number object with mediaQueries', () => {
-  const render = renderer
-    .create(
-      <ThemeProvider theme={{ ...theme, mediaQueries }}>
-        <NumberObjectDiv>{content}</NumberObjectDiv>
-      </ThemeProvider>
-    )
-    .toJSON()
-  expect(render).toMatchSnapshot()
-})
+    expect(render).toMatchSnapshot()
+  })
 
-it('renders string object with mediaQueries', () => {
-  const render = renderer
-    .create(
-      <ThemeProvider theme={{ ...theme, mediaQueries }}>
-        <StringObjectDiv>{content}</StringObjectDiv>
-      </ThemeProvider>
-    )
-    .toJSON()
-  expect(render).toMatchSnapshot()
-})
+  test(`${name} renders mixed object values using breakpoints`, () => {
+    const Div = styled.div`
+      ${fn(
+        JSON.parse(
+          `{${faker.fake(
+            '"smallCustomBreakpoint": "{{random.number}}", "mobile": "{{date.month}}", "mediumCustomBreakpoint": "{{random.number}}", "tablet": "{{date.month}}", "desktop": "{{random.number}}", "wide": "{{date.month}}", "largeCustomBreakpoint": "{{random.number}}", "standard": "{{date.month}}"'
+          )}}`
+        )
+      )}
+    `
 
-it('renders mixed object with mediaQueries', () => {
-  const render = renderer
-    .create(
-      <ThemeProvider theme={{ ...theme, mediaQueries }}>
-        <MixedObjectDiv>{content}</MixedObjectDiv>
-      </ThemeProvider>
-    )
-    .toJSON()
-  expect(render).toMatchSnapshot()
-})
+    const render = renderer
+      .create(
+        <ThemeProvider theme={{ ...theme, breakpoints }}>
+          <Div>{faker.random.words()}</Div>
+        </ThemeProvider>
+      )
+      .toJSON()
+
+    expect(render).toMatchSnapshot()
+  })
+
+  test(`${name} renders number object values using mediaQueries`, () => {
+    const Div = styled.div`
+      ${fn(
+        JSON.parse(
+          `{${faker.fake(
+            '"smallCustomBreakpoint": {{random.number}}, "mobile": {{random.number}}, "mediumCustomBreakpoint": {{random.number}}, "tablet": {{random.number}}, "desktop": {{random.number}}, "wide": {{random.number}}, "largeCustomBreakpoint": {{random.number}}, "standard": {{random.number}}'
+          )}}`
+        )
+      )}
+    `
+
+    const render = renderer
+      .create(
+        <ThemeProvider theme={{ ...theme, mediaQueries }}>
+          <Div>{faker.random.words()}</Div>
+        </ThemeProvider>
+      )
+      .toJSON()
+
+    expect(render).toMatchSnapshot()
+  })
+
+  test(`${name} renders string object values using mediaQueries`, () => {
+    const Div = styled.div`
+      ${fn(
+        JSON.parse(
+          `{${faker.fake(
+            '"smallCustomBreakpoint": "{{date.month}}", "mobile": "{{date.month}}", "mediumCustomBreakpoint": "{{date.month}}", "tablet": "{{date.month}}", "desktop": "{{date.month}}", "wide": "{{date.month}}", "largeCustomBreakpoint": "{{date.month}}", "standard": "{{date.month}}"'
+          )}}`
+        )
+      )}
+    `
+
+    const render = renderer
+      .create(
+        <ThemeProvider theme={{ ...theme, mediaQueries }}>
+          <Div>{faker.random.words()}</Div>
+        </ThemeProvider>
+      )
+      .toJSON()
+
+    expect(render).toMatchSnapshot()
+  })
+
+  test(`${name} renders mixed object values using mediaQueries`, () => {
+    const Div = styled.div`
+      ${fn(
+        JSON.parse(
+          `{${faker.fake(
+            '"smallCustomBreakpoint": "{{random.number}}", "mobile": "{{date.month}}", "mediumCustomBreakpoint": "{{random.number}}", "tablet": "{{date.month}}", "desktop": "{{random.number}}", "wide": "{{date.month}}", "largeCustomBreakpoint": "{{random.number}}", "standard": "{{date.month}}"'
+          )}}`
+        )
+      )}
+    `
+
+    const render = renderer
+      .create(
+        <ThemeProvider theme={{ ...theme, mediaQueries }}>
+          <Div>{faker.random.words()}</Div>
+        </ThemeProvider>
+      )
+      .toJSON()
+
+    expect(render).toMatchSnapshot()
+  })
+}
